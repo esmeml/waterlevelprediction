187a59d0a4dc909a74b1b98bb34f96bcd605279d
NoaNova <noarachdihebert@gmail.com>
2025-05-23 00:29:19 +0200

    PREDICTIONS

--- projet_streamlit/app5.py
+++ projet_streamlit/app5.py
@@ -32,7 +32,8 @@ import warnings
 st.set_page_config(page_title="Observations Hydrologiques", layout="wide")
 st.title("Information sur les cours d’eau")
 
-DOSSIER_JSON = r"C:\Users\niels\Git\waterlevelprediction\projet_streamlit\data"
+#DOSSIER_JSON = r"C:\Users\niels\Git\waterlevelprediction\projet_streamlit\data"
+DOSSIER_JSON = r"C:\Users\noara\Git\waterlevelprediction\projet_streamlit\data"
 
 # Lister les fichiers JSON
 fichiers = [f for f in os.listdir(DOSSIER_JSON) if f.endswith(".json")]
